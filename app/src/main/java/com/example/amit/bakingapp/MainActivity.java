ca38c4c6d44e982c1d103521064e0a292cf8ea23
Amit Grover <amitg90@gmail.com>
2018-08-01 23:25:54 -0700

    Fixed Minor Issues

--- app/src/main/java/com/example/amit/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/amit/bakingapp/MainActivity.java
@@ -11,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
